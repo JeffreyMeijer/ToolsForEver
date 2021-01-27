66b7e43a1c9d32d12015c5cc845b0c1958331733
Jeffrey <123974@student.drenthecollege.nl>
2021-01-27 11:43:48 +0100

    Begin met benodigdheden voor locatie toevoegen

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -21,12 +21,12 @@
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
-                ToolsForEver heeft 
+                De voorraad van alle ToolsForEver distributiecentrums kun je hier bekijken
             </div>
 
             <a href="{{route('products')}}">
                 <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
-                        <div>Explore the documentation</div>
+                        <div>Bekijk de vooraad bij ToolsForEver</div>
 
                         <div class="ml-1 text-indigo-500">
                             <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -44,12 +44,12 @@
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
-                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+            Bekijk de medewerkers
             </div>
 
             <a href="{{route('medewerkers')}}">
                 <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
-                        <div>Start watching Laracasts</div>
+                        <div>Bekijk de medewerkers bij ToolsForEver</div>
 
                         <div class="ml-1 text-indigo-500">
                             <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
